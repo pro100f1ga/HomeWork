e34a5110c8f993745bd0fef00896870657febbd7
f1ga <welcometotheclubbuddy89@gmail.com>
2023-09-09 19:56:24 +0300

    4ая домашка

---
+++ Homework04/HW04_Ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int Exponent(int number, int power)
+{
+    int result = 1;
+    for (int i = 0; i < power; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+bool ValidateExponent(int power)
+{
+    if (power < 0)
+    {
+        System.Console.WriteLine("Степень не должна быть равна 0");
+        return false;
+    }
+    return true;
+
+}
+
+int Number = InputNum("Введите число: ");
+int power = InputNum("Введите степень: ");
+int result = Exponent(Number, power);
+if (ValidateExponent(power))
+{
+    Console.WriteLine($"{Number}^{power} = {result}");
+}
+
---
+++ Homework04/HW04_Ex02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumDigit(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+int num = InputNum("Введите число: ");
+Console.WriteLine($"Сумма всех цифр в числе {num} равна {SumDigit(num)}");
