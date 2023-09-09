1d7f0b3af8e0467017bbe28fcf0af512bfd2da6a
f1ga <welcometotheclubbuddy89@gmail.com>
2023-09-09 13:53:16 +0300

    Домашка 3

---
+++ Homework03/Ex01/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+
+Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+
+Для остальных чисел вернуть True или False.
+*/
+
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+       if (number < 10000 || number > 99999)
+        {
+            Console.WriteLine("Число не пятизначное");
+            return false;
+        }
+        int reverse = 0;
+        int originalNumber = number;
+
+        while (number != 0)
+        {
+            int lastDigit = number % 10;
+            reverse = (reverse * 10) + lastDigit;
+            number /= 10;
+        }
+
+        return originalNumber == reverse;
+    }
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64566;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
---
+++ Homework03/Ex03/Program.cs
@@ -0,0 +1,35 @@
+﻿//Внутри класса Answer напишите метод ShowCube, который принимает на вход число (N) и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+using System;
+public class Answer
+{
+    static void ShowCube(int N)
+    {
+        if (N < 1)
+        {
+            Console.WriteLine("N должно быть положительным целым числом.");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++)
+        {
+            int cube = i * i * i;
+            Console.WriteLine($"Куб {i} равен {cube}");
+        }
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int N;
+
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            N = 6;
+        }
+
+        // Не удаляйте строки ниже
+        ShowCube(N);
+    }
+}
