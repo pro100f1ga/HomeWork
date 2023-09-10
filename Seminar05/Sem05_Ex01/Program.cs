2a57c4964a6cd92b22b2b01bd4171834d144251e
f1ga <welcometotheclubbuddy89@gmail.com>
2023-09-10 12:45:25 +0300

    семинар 5

---
+++ Seminar05/Sem05_Ex01/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+// 1ый способ
+/*
+void FillArray( int[] ints)
+{   
+    Random rnd = new Random();
+    for (int i = 0; i < ints.Length; i++)
+        ints[i]= new Random().Next(-9,10);
+}
+
+void PrintArray (int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine($"{arr[i]}");
+    }
+    System.Console.WriteLine();
+}
+
+void SumPosNegNums(int[] arr)
+{
+    int posSum = 0;
+    int negSum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]>0)
+            posSum += arr[i]; // posSum = posSum + arr[i];
+        else
+            negSum += arr[i];
+    }
+    System.Console.WriteLine($"Сумма положительных чисел равна {posSum}");
+    System.Console.WriteLine($"Сумма отрицательных чисел равна {negSum}");
+
+}
+
+int[] newArray = new int[12];
+FillArray(newArray);
+PrintArray(newArray);
+SumPosNegNums(newArray);
+*/
+
+// 2ой способ
+
+void FillArray(int[] ints)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < ints.Length; i++)
+        ints[i] = rnd.Next(-9, 10);
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+
+int[] SumPosNegNums(int[] arr)
+{
+    int[] sums = new int[2];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            sums[0] += arr[i]; // posSum = posSum + arr[i];
+        else
+            sums[1] += arr[i];
+    }
+    return sums;
+}
+
+int[] newArray = new int[12];
+FillArray(newArray);
+PrintArray(newArray);
+int[] result = SumPosNegNums(newArray);
+PrintArray(result);
---
+++ Seminar05/Sem05_Ex05/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int InputNum(string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+return new int[size];
+}
+
+void FillArray(int[] array, int minValue, int maxValue)
+{
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+array[i] = rnd.Next(minValue, maxValue + 1);
+}
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+Console.Write($"{array[i]} ");
+Console.WriteLine();
+}
+
+int[] MultPairs(int[] array)
+{
+int size = array.Length / 2 + array.Length % 2;
+// int size = array.Length / 2;
+// if (array.Length % 2 == 1) size++;
+int[] newArr = new int[size];
+
+for (int i = 0; i < array.Length / 2; i++)
+newArr[i] = array[i] * array[array.Length - 1 - i];
+
+if (array.Length % 2 == 1)
+newArr[size - 1] = array[array.Length / 2];
+return newArr;
+}
+
+int size = InputNum("Введите размер массива: ");
+int[] ints = CreateArray(size);
+
+int min = InputNum("Введите минимальное значение элемента массива: ");
+int max = InputNum("Введите максимальное значение элемента массива: ");
+
+FillArray(ints, min, max);
+PrintArray(ints);
+
+int[] result = MultPairs(ints);
+PrintArray(result);
