9099e9043bfb07475a4599937ee3ab5e515300d0
f1ga <welcometotheclubbuddy89@gmail.com>
2023-09-21 15:11:17 +0300

    изменения семинар7

--- Seminar07/ex01/Program.cs
+++ Seminar07/ex01/Program.cs
@@ -5,31 +5,31 @@
 // 77 3 8 1
 int InputNum(string message)
 {
-Console.Write(message);
-return int.Parse(Console.ReadLine()!);
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
 }
 
 int[,] Create2DArray(int rows, int columns)
 {
-return new int[rows, columns];
+    return new int[rows, columns];
 }
 
 void Fill2DArray(int[,] ints, int minValue, int maxValue)
 {
-Random rnd = new Random();
-for (int i = 0; i < ints.GetLength(0); i++)
-for (int j = 0; j < ints.GetLength(1); j++)
-ints[i, j] = rnd.Next(minValue, maxValue + 1); // Next(2, 5) -> [2, 5)
+    Random rnd = new Random();
+    for (int i = 0; i < ints.GetLength(0); i++)
+        for (int j = 0; j < ints.GetLength(1); j++)
+            ints[i, j] = rnd.Next(minValue, maxValue + 1); // Next(2, 5) -> [2, 5)
 }
 
 void Print2DArray(int[,] ints)
 {
-for (int i = 0; i < ints.GetLength(0); i++)
-{
-for (int j = 0; j < ints.GetLength(1); j++)
-Console.Write($"{ints[i, j]}\t");
-Console.WriteLine();
-}
+    for (int i = 0; i < ints.GetLength(0); i++)
+    {
+        for (int j = 0; j < ints.GetLength(1); j++)
+            Console.Write($"{ints[i, j]}\t");
+        Console.WriteLine();
+    }
 }
 
 int rows = InputNum("Введите количество строк: ");
--- Seminar07/ex02/Program.cs
+++ Seminar07/ex02/Program.cs
@@ -6,37 +6,37 @@
 
 int InputNum(string message)
 {
-Console.Write(message);
-return int.Parse(Console.ReadLine()!);
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
 }
 
 int[,] CreateArr(int first, int second)
 {
-int[,] array = new int[first, second];
-return array;
+    int[,] array = new int[first, second];
+    return array;
 }
 
 void FillArray(int[,] arr)
 {
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-for (int j = 0; j < arr.GetLength(1); j++)
-{
-arr[i, j] = i + j;
-}
-}
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
 }
 
 void PrintArray(int[,] arr)
 {
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-for (int j = 0; j < arr.GetLength(1); j++)
-{
-Console.Write(arr[i, j] + " ");
-}
-Console.WriteLine();
-}
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
 }
 int rows = InputNum("Введите количество строк: ");
 int columns = InputNum("Введите количество столбцов: ");
